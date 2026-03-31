d5520745f68ce7fad1d84a8d6715d9604504a95c
ostp.echoshift <ostp.echoshift@gmail.com>
2026-03-31 12:48:07 -0600

    OSTP @echoShift - Landing Page
    # OSTP @echoShift - Landing Page
    
    **Operador:** Chavalow - OSTP @echoShift - Zapopan, Jalisco MX
    **Pipeline:** Leviatan A-E
    **Live:** https://www.ostp-echoshift.com
    **Repo:** github.com/ostp-echoshift/ostp-echoshift
    **Build:** 2026-03-31 11:03:09

---
+++ js/reveal.js
@@ -0,0 +1,13 @@
+﻿// reveal.js - OSTP @echoShift - Nivel C
+var revealObs = new IntersectionObserver(function(entries) {
+    entries.forEach(function(e) {
+        if (e.isIntersecting) {
+            e.target.classList.add('visible');
+            revealObs.unobserve(e.target);
+        }
+    });
+}, { threshold: 0.12 });
+
+document.querySelectorAll('.reveal').forEach(function(el) {
+    revealObs.observe(el);
+});
